7a59a3da4fcc22839487566ca6296bf44866542b
Kharchevna <48067522+Kharchevna@users.noreply.github.com>
2020-02-06 13:58:58 +0200

    Updates

---
+++ precache-manifest.08e6b13cec4366c2ec9b4e80e21a0bdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d54623cd9eb6ef799cc2ebd2c87aee",
+    "url": "/React_HW_Transactions/index.html"
+  },
+  {
+    "revision": "7eeec2f653063c9e02f9",
+    "url": "/React_HW_Transactions/static/css/main.58b70171.chunk.css"
+  },
+  {
+    "revision": "c15715256586936995c7",
+    "url": "/React_HW_Transactions/static/js/2.c126fbac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_HW_Transactions/static/js/2.c126fbac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7eeec2f653063c9e02f9",
+    "url": "/React_HW_Transactions/static/js/main.3a4b6b66.chunk.js"
+  },
+  {
+    "revision": "7c132a35c6cc68ae1c35",
+    "url": "/React_HW_Transactions/static/js/runtime-main.be8c44b7.js"
+  }
+]);
